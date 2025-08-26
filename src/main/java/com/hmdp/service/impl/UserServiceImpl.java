9c1899d5892a93a3ae059c1de0b7d9ae4a15bddf
ianSama0217 <ian20000217@gmail.com>
2025-08-26 22:56:49 +0800

    sendCode api

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,9 +1,17 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+
+import cn.hutool.core.util.RandomUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,7 +22,27 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 檢查手機號碼格式
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手機號碼格式錯誤");
+        }
+
+        // 生成驗證碼
+        String code = RandomUtil.randomNumbers(6);
+
+        // 保存驗證碼到session
+        session.setAttribute("code", code);
+
+        // 發送驗證碼
+        log.debug("驗證碼已發送，驗證碼：{}", code);
+
+        return Result.ok();
+    }
 
 }
