955e88665a4ca6b29cc38c9247f2804b14522ecd
ianSama0217 <ian20000217@gmail.com>
2025-08-26 23:42:45 +0800

    login api

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -3,6 +3,7 @@ package com.hmdp.service;
 import javax.servlet.http.HttpSession;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
@@ -17,5 +18,7 @@ import com.hmdp.entity.User;
 public interface IUserService extends IService<User> {
 
     Result sendCode(String phone, HttpSession session);
+
+    Result login(LoginFormDTO loginForm, HttpSession session);
 
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
 
 import cn.hutool.core.util.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +43,40 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         // 發送驗證碼
         log.debug("驗證碼已發送，驗證碼：{}", code);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 檢查手機號碼格式
+        String phone = loginForm.getPhone();
+
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手機號碼格式錯誤");
+        }
+
+        // 檢查驗證碼
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("驗證碼錯誤");
+        }
+
+        // 根據手機號碼查詢用戶是否存在
+        User user = query().eq("phone", phone).one();
+
+        // 不存在則創建新用戶
+        if (user == null) {
+            user = new User();
+            user.setPhone(phone);
+            user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+            save(user);
+        }
+
+        // 保存用戶信息到session
+        session.setAttribute("user", user);
 
         return Result.ok();
     }
