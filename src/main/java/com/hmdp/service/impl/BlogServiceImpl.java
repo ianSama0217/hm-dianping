dd97b4674de497bbb435c762d749aa0b35d8d89f
ianSama0217 <ian20000217@gmail.com>
2025-10-19 15:12:29 +0800

    blog api 新增貼文後, 將通知推送到粉絲收件箱

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -3,9 +3,11 @@ package com.hmdp.service.impl;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
+import com.hmdp.service.IFollowService;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.SystemConstants;
@@ -42,6 +44,9 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private IFollowService followService;
 
     @Override
     public Result queryHotBlog(Integer current) {
@@ -150,6 +155,28 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         String key = RedisConstants.BLOG_LIKED_KEY + blog.getId();
         Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
         blog.setIsLike(score != null);
+    }
+
+    @Override
+    public Result saveBlog(Blog blog) {
+        // 取得登入用戶
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店貼文
+        boolean isSuccess = save(blog);
+        if (!isSuccess) {
+            return Result.fail("新增貼文失敗");
+        }
+        // 查詢貼文作者的所有粉絲
+        List<Follow> follows = followService.query().eq("follow_user_id", user.getId()).list();
+        // 推送貼文給所有粉絲
+        for (Follow follow : follows) {
+            Long userId = follow.getUserId();
+            String key = RedisConstants.FEED_KEY + userId;
+            stringRedisTemplate.opsForZSet().add(key, blog.getId().toString(), System.currentTimeMillis());
+        }
+        // 返回id
+        return Result.ok(blog.getId());
     }
 
 }
